bdb28189b5140903a175d2597c807fa444582d65
esmaleva <smaleva.evgenia@yandex.com>
2018-07-25 15:10:11 +0300

    +

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-$('.menu-btn').on('click', function(e) {
+$('.menu-btn').on('click', function (e) {
   e.preventDefault;
   $(this).toggleClass('menu-btn_active');
   $('.nav').toggleClass('flex');
   $('.nav').toggleClass('none');
 });
-$('.lk-link').on('click', function(e) {
-    e.preventDefault;
-    
-    $('.submenu').toggleClass('flex');
-    $('.submenu').toggleClass('none');
-  });
+$('.lk-link').on('click', function (e) {
+  e.preventDefault;
+
+  $('.submenu').toggleClass('flex');
+  $('.submenu').toggleClass('none');
+});
+
+$('.field-wrap').find('input').on('keyup blur focus', function (e) {
+
+  var $this = $(this),
+    label = $this.prev('label');
+
+  if (e.type === 'keyup') {
+    if ($this.val() === '') {
+      label.removeClass('active');
+    } else {
+      label.addClass('active');
+    }
+  } else if (e.type === 'blur') {
+    if ($this.val() === '') {
+      label.removeClass('active');
+    }
+  } else if (e.type === 'focus') {
+
+    if ($this.val() === '') {
+      label.removeClass('active');
+    }
+    else if ($this.val() !== '') {
+      label.addClass('active');
+    }
+  }
+});
